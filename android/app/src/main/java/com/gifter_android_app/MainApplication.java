e6dcae90af9225a42d596b2af7032ab69f85f99e
Verkhohliad <ihor.verkhohliad@gmail.com>
2020-06-12 18:39:03 +0300

    [AUTHENTICATION] settings up facebook for android, install rn-fbsdk

--- android/app/src/main/java/com/gifter_android_app/MainApplication.java
+++ android/app/src/main/java/com/gifter_android_app/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -43,6 +45,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
