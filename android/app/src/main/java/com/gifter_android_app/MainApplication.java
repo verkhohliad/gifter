a0e20da21926bc23ef8b4ec6dba0ae25cf5f3dd8
Verkhohliad <ihor.verkhohliad@gmail.com>
2020-06-22 15:59:49 +0300

    [AUTHENTICATION] try to fix android facebook auth (decided to fix later)

--- android/app/src/main/java/com/gifter_android_app/MainApplication.java
+++ android/app/src/main/java/com/gifter_android_app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
 
-          package.add(new VectorIconsPackage());
+//           packages.add(new VectorIconsPackage());
           return packages;
         }
 
