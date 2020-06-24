3ef9527345d35b4da3847aac42178c01e74ec5ed
Verkhohliad <ihor.verkhohliad@gmail.com>
2020-06-24 20:36:59 +0300

    [AUTH-VK] fix ios build, start trying vk auth

--- src/screens/CalendarScreen/CalendarScreen.js
+++ src/screens/CalendarScreen/CalendarScreen.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
 
 import styles from './styles';
 
+// todo: VK login rewrite
+// eslint-disable-next-line max-len
+const VK_LOGIN_URL = 'https://oauth.vk.com/authorize?client_id=7520956&display=page&redirect_uri=https://gifter-38720.firebaseapp.com/__/auth/handler&scope=friends&response_type=code&v=5.110';
+
 const CalendarScreen = () => {
+  const [isWebViewOpen, setIsWebViewOpen] = React.useState(false);
+  const vkLogin = React.useCallback(() => {
+    setIsWebViewOpen(true);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Calendar Screen</Text>
+
+      <Button
+        title="Login with VK"
+        onPress={vkLogin}
+      />
+
+      {isWebViewOpen && (
+        <WebView
+          source={{ uri: VK_LOGIN_URL }}
+        />
+      )}
     </SafeAreaView>
   );
 };
