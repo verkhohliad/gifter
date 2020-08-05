2bad838135d8248fba8e9c0b1ebf3683bc2e5364
Ihor Verkhohliad <ihor.verkhohliad@gmail.com>
2020-08-05 16:53:21 +0300

    Remove functions from repo and stuff with vk service (#6)
    * [AUTH] remove functions, delete vk examples usage
    
    * [AUTH] add google vars to the gitignore
    
    * [SKIP AUTH] commit staied changes

--- src/services/vk/examples.js
+++
@@ -1,5 +0,0 @@
-
-// eslint-disable-next-line max-len
-export const VK_LOGIN_URL = 'https://oauth.vk.com/authorize?client_id=7520956&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.110';
-// eslint-disable-next-line max-len
-export const VK_FETCH_FRIENDS_FIELDS = 'https://api.vk.com/method/friends.get?user_id={accessData.user_id}&fields=bdate&access_token={accessData.access_token}&v=5.110';
